bb744964397fb3629fd06d769cb357cd3cb550a4
SilasScott <silas.scott@soprasteria.com>
2019-12-26 16:48:05 +0100

    DAO's et DAOHibernate

---
+++ catan-JPA/src/main/java/fr/formation/DAO/hibernate/DAOCroisementHibernate.java
@@ -0,0 +1,5 @@
+package fr.formation.DAO.hibernate;
+
+public class DAOCroisementHibernate {
+
+}
---
+++ catan-JPA/src/main/java/fr/formation/DAO/hibernate/DAOHibernate.java
@@ -0,0 +1,40 @@
+package fr.formation.DAO.hibernate;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public abstract class DAOHibernate {
+	
+	protected static EntityManagerFactory emf = null;
+	protected EntityManager em = null;
+	
+	
+	public DAOHibernate() {
+		if(emf == null) {
+			emf = Persistence.createEntityManagerFactory("CatanPersistenceUnit");
+		}
+		
+		if (emf != null){
+			this.em = emf.createEntityManager();
+		}
+
+		
+
+	}
+	public static void close() {
+		if (emf != null){
+			emf.close();
+			emf = null;
+		}
+	}
+
+	public void setEMF() {
+
+		
+	
+}
+
+
+
+}
