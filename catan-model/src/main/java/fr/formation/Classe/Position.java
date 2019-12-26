0e4480d5ab5d483b5a836aa136bf12d5689e85d3
marcellinsimon <marcellin.simon@soprasteria.com>
2019-12-26 15:19:31 +0100

    classe position

--- catan-model/src/main/java/fr/formation/Classe/Position.java
+++ catan-model/src/main/java/fr/formation/Classe/Position.java
@@ -1,9 +1,30 @@
 package fr.formation.Classe;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name="position")
 public class Position {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="POS_ID")
 	private int id;
+	@Column(name = "POS_POS")
+	@Size(max = 2)
 	private int pos;
+	@Column(name = "POS_VALEUR")
+	@Size(max = 2)
 	private int val;
+	@Column(name = "POS_TYPE")
+	@Enumerated(EnumType.STRING)
 	private Type type;
 	
 	public int getId() {
