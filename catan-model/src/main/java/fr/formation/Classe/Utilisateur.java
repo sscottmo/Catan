d46efbc9591f1a3fb156dedf0cf7db228c4f82da
marcellinsimon <marcellin.simon@soprasteria.com>
2019-12-26 15:06:45 +0100

    getters + setters utilisateur

--- catan-model/src/main/java/fr/formation/Classe/Utilisateur.java
+++ catan-model/src/main/java/fr/formation/Classe/Utilisateur.java
@@ -29,5 +29,38 @@ public abstract class Utilisateur {
 	
 	@Column(name="UTI_EST_CONNECTE")
 	private boolean estConnecte;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getMotDePasse() {
+		return motDePasse;
+	}
+
+	public void setMotDePasse(String motDePasse) {
+		this.motDePasse = motDePasse;
+	}
+
+	public boolean isEstConnecte() {
+		return estConnecte;
+	}
+
+	public void setEstConnecte(boolean estConnecte) {
+		this.estConnecte = estConnecte;
+	}
+	
 	
 }
