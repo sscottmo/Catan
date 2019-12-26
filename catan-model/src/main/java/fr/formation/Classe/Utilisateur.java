717e4a6f414ac6b4db464dcf6e58892881465f3c
marcellinsimon <marcellin.simon@soprasteria.com>
2019-12-26 15:02:48 +0100

    attribut classe utilisateur

--- catan-model/src/main/java/fr/formation/Classe/Utilisateur.java
+++ catan-model/src/main/java/fr/formation/Classe/Utilisateur.java
@@ -1,7 +1,33 @@
 package fr.formation.Classe;
 
-public class Utilisateur {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name="utilisateur")
+public abstract class Utilisateur {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="UTI_ID")
 	private int id;
+
+	@Column(name="UTI_PSEUDO",length = 100, nullable = false)
+	@NotEmpty
+	@Size(max = 100)
 	private String nom;
-	private Partie partie;
+
+	@Column(name="UTI_MOT_DE_PASSE",length = 100, nullable = false)
+	@NotEmpty
+	@Size(max = 100)
+	private String motDePasse;
+	
+	@Column(name="UTI_EST_CONNECTE")
+	private boolean estConnecte;
+	
 }
