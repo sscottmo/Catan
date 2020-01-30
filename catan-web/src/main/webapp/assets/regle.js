02c0fa0dfb699fa0704d8a9489e3430ebbca4c54
marcellinsimon <marcellin.simon@soprasteria.com>
2020-01-30 15:35:12 +0100

    script

--- catan-web/src/main/webapp/assets/regle.js
+++ catan-web/src/main/webapp/assets/regle.js
@@ -4,11 +4,11 @@ function lanceDe() {
 	alert(Math.floor(Math.random()*5 + Math.random()*5 + 2));
 }
 
-function desactive_bouton()
-{
+function desactive_bouton() {
  
 	document.getElementById('lanceDe').value="Fin de tour";
 	document.getElementById('lanceDe').style.display="none";
+	document.getElementById('findetour').style.display="block";
 	
 }
 
@@ -17,9 +17,29 @@ document.querySelector('#lanceDe')
 		.addEventListener('click', (event) =>{
 			lanceDe();
 			desactive_bouton();   
-			document.getElementById('findetour').style.display="block";
 		});
 
+function ajouteCroisement(id,couleur) {
+	document.querySelector(id)
+			.style.backgroundColor=couleur;
+}
+
+//for (i=1 ; i<55 ; i++) {
+//	let id = '#cr_'+ i.toString();
+//	document.querySelector(id)
+//			.addEventListener('click', (event) => {
+//				ajouteCroisment()
+//				});
+//}
+
+var ad = 'http://localhost:8080/catan-web/plateau.html';
+var req = new XMLHttpRequest();
+	
+//document.querySelector('p')
+//		.addEventListener('click', (event) =>{
+//			document.querySelector(this.getAttribute('id'))
+//					.background-color('black')
+//});
 
 document.querySelector('#findetour')
 		.addEventListener('click', (event) =>{
@@ -29,5 +49,14 @@ document.querySelector('#findetour')
 		});
 
 
-//[[${sessionJoueur.bois}]]
+let eventSource = new EventSource('http://172.16.44.102:8080/catan-web/api/plateau/sse');
 
+eventSource.addEventListener('message', (croisement,couleur) => {
+	
+	let produit = {
+			libelle: event.data,
+			prix: 0
+	};
+	afficheProduitURL(produit);
+	
+})
